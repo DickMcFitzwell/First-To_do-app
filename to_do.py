1bbe0dca19111f239e2f5b36cf01c0922b91c739
DickMcFitzwell <dvrealtytampa@gmail.com>
2025-07-05 01:41:32 -0400

    Version 0.3 - added complete_task function, added changelog and roadmap

--- to_do.py
+++ to_do.py
@@ -13,16 +13,28 @@ def main():
             print(task)
         print("")
 
-    mode = input("Would you like to \"add\" or \"del\" a task?\n").strip().lower()
+    mode = input("Would you like to \"add\", \"complete\", or \"delete\" a task?\n").strip().lower()
     
     if mode == "add":
         n = input("New Task?\n")
         if n:
-            tasks.append(Task(n))
-            save_tasks(tasks)
-            tasks = load_tasks()
+            if any(t.description.lower() == n.strip().lower() for t in tasks):
+                print(f"{n} already exists\n")
+                print("")
+                return
+            else:
+                tasks.append(Task(n))
+                save_tasks(tasks)
+                tasks = load_tasks()
         os.system("cls")
-    elif mode == "del":
+    elif mode == "complete":
+        if not tasks:
+            print("No tasks to complete\n")
+            return
+        n = input("Task to mark complete?\n")
+        complete_task(n)
+        tasks = load_tasks()
+    elif mode == "delete":
         if not tasks:
             print("No tasks to delete\n")
             return
@@ -50,7 +62,7 @@ class Task:
 
     def __str__(self):
         status = "Completed" if self.is_complete else "Incomplete"
-        return f"{self.description} [{status}]"
+        return f"[{status}] {self.description}"
 
     def mark_complete(self):
         self.is_complete = True
@@ -93,15 +105,47 @@ def delete_task(task, filename = "tasks.json"):
         print("")
         return 
     saved_tasks = [Task.from_dict(d) for d in saved_tasks]
-    filtered_tasks = [t for t in saved_tasks if t.description.lower() != task.lower()]
-    if len(filtered_tasks) == len(saved_tasks):
+    target_task = next((t for t in saved_tasks if t.description.lower().startswith(task.lower())), None)
+    if target_task is None:
         print("Task not found")
         print("")
         return
     else:
-        save_tasks(filtered_tasks, filename)
-        print(f"{task} deleted!")
+        saved_tasks.remove(target_task)
+        save_tasks(saved_tasks, filename)
+        print(f"{target_task.description} deleted!")
         print("")
+
+
+def complete_task(task, filename = "tasks.json"):
+    try:
+        with open(filename, "r") as file:
+            saved_tasks = json.load(file)
+    except FileNotFoundError:
+        print("File not found\n")
+        print("")
+        return
+    saved_tasks = [Task.from_dict(d) for d in saved_tasks]
+    found = False
+    for t in saved_tasks:
+        if t.description.lower().startswith(task.lower()):
+            if t.is_complete == True:
+                print(f"{t.description} is already completed\n")
+                print("")
+                return
+            t.mark_complete()
+            found = True
+            break
+    if found:
+        with open(filename, "w") as file:
+            json.dump([t.to_dict() for t in saved_tasks], file, indent=2)
+            print(f"{t.description} marked complete\n")
+            print("")
+    else:
+        print("Task not found\n")
+        print("")
+
+
 
 
 
